b49a80b2d1ca41a32b8fdde10f8fe96e5954456c
L1za-T <linasp93@gmail.com>
2024-02-03 23:13:31 +0000

    Continued q1. Can push to stack, has trouble recognising negative numbers.

--- CA3_Starter_Code/src/CA3_Question1.java
+++ CA3_Starter_Code/src/CA3_Question1.java
@@ -7,23 +7,10 @@ import java.util.Scanner;
  */
 public class CA3_Question1
 {
-    static int license;
-    Scanner kbInput = new Scanner(System.in);
 
-    Stack<Integer> driveway = new Stack<Integer>();
-    Stack<Integer> street = new Stack<Integer>();
-
-
-    static void drivewayPush(Stack<Integer> driveway){
-        driveway.push(license);
-    }
 
     static void drivewayPop(Stack<Integer> driveway){
         driveway.pop();
-    }
-
-    static void streetPush(Stack<Integer> street){
-        street.push(license);
     }
 
     static void streetPop(Stack<Integer> street){
@@ -32,6 +19,29 @@ public class CA3_Question1
 
     public static void runSimulation()
     {
+        int license;
+
+        Scanner kbInput = new Scanner(System.in);
+
+        Stack<Integer> driveway = new Stack<Integer>();
+
+        Stack<Integer> street = new Stack<Integer>();
+
+        System.out.println("Please enter the license number of the parked car.");
+        do{
+            license = kbInput.nextInt();
+            if(license > 0){
+                driveway.push(license);
+                System.out.println(driveway);
+            } else if (license < 0) {
+                System.out.println(driveway.search(license));
+                System.out.println(driveway);
+            } else if (license == 0){
+                System.out.println(driveway);
+                break;
+            }
+
+        } while (license != 0);
 
     }
 
