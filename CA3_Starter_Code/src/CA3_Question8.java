4210ea00a02cd1444b89fc9595c0db2d5966ef58
L1za-T <linasp93@gmail.com>
2024-02-09 18:18:08 +0000

    Q5 working. Q8 done in theory but needs debugging.

--- CA3_Starter_Code/src/CA3_Question8.java
+++ CA3_Starter_Code/src/CA3_Question8.java
@@ -8,25 +8,55 @@ import java.util.Stack;
 public class CA3_Question8 {
 
 
-    public Stack<Integer> numbers = new Stack<>();
-    public Stack<String> operators = new Stack<>();
+    public static Stack<Double> numbers = new Stack<>();
+    public static Stack<String> operators = new Stack<>();
+    public static char[] hPOp = {'*', '/'};
+    public static char[] lPOp = {'+', '-'};
     /*
         Reads in an equation from the user
      */
-    public double result;
+    public static double result;
 
-    public double calc (String equation){
+    public static double calc(String equation){
+        for(int i = 0; i < equation.length(); i++){
+            char checkChar = equation.charAt(i);
+            Character numCheck = equation.charAt(i);
 
+            if(numCheck.isDigit(checkChar)){
+                numbers.push(Double.parseDouble(numCheck.toString()));
+
+            } else if (checkChar == '(') {
+                operators.push(numCheck.toString());
+
+            } else if (checkChar == hPOp[0] || checkChar == hPOp[1] || checkChar == lPOp[0] || checkChar == lPOp[1]) {
+                System.out.println("Crash test");
+                while (operators.peek().charAt(0) == hPOp[0] || operators.peek().charAt(0) == hPOp[1] && checkChar == lPOp[0] || checkChar == lPOp[1]){
+                    evaluateTop();
+                }
+                operators.push(numCheck.toString());
+            } else if (checkChar == ')') {
+                while (!operators.peek().equals("(")){
+                    evaluateTop();
+                }
+
+                operators.pop();
+            }
+        }
+        while (!operators.isEmpty()){
+            evaluateTop();
+        }
 
         return result;
     }
 
-    public void evaluateTop(){
-        int number = 0;
-        int x = numbers.pop();
-        int y = numbers.pop();
+    public static void evaluateTop(){
+        double number = 0;
+
+        double x = numbers.pop();
+        double y = numbers.pop();
 
         String operator = operators.pop();
+        System.out.println("Wave");
 
         if (operator.trim().equals("*")||operator.trim().equalsIgnoreCase("x")){
             number = y*x;
@@ -45,5 +75,7 @@ public class CA3_Question8 {
         Scanner in = new Scanner(System.in);
         System.out.println("Please enter equation");
         equation = in.nextLine().trim();
+        calc(equation);
+        System.out.println("The result is: " + result);
     }
 }
